cf2d57e432f3c133061f822ba9956443408e2274
jason <xjason.rogersx@gmail.com>
2026-02-18 20:54:02 +0000

    dynamic loading

--- level.gd
+++ level.gd
@@ -2,9 +2,31 @@ extends Node3D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	load_gltf_segments("res://fce41ff62f8a_gltf/", 8)
 
+func load_gltf_segments(path: String, count: int):
+	var gltf_document = GLTFDocument.new()
+	var gltf_state = GLTFState.new()
+	
+	for i in range(1, count + 1):
+		var file_name = "fce41ff62f8a_" + str(i) + "-col.gltf"
+		var full_path = path + file_name
+		print("loading " + file_name)
+		
+		var node = load( full_path ).instantiate()
+		
+		$World.add_child(node)
+		
+		# Force generation if -col suffix fails
+		if node is MeshInstance3D:
+			node.create_trimesh_collision() 
+		else:
+			# If the GLTF is a scene, find the meshes inside
+			for child in node.find_children("*", "MeshInstance3D"):
+				child.create_trimesh_collision()
+		
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
