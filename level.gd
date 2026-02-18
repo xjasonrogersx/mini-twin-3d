6f58b426e8518e5b568242213aa7ec63a7b8d6cb
jason <xjason.rogersx@gmail.com>
2026-02-18 21:11:40 +0000

    dynamic loading larger area

--- level.gd
+++ level.gd
@@ -3,14 +3,14 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	load_gltf_segments("res://fce41ff62f8a_gltf/", 8)
+	load_gltf_segments("res://e291f2784ca3_gltf/", 64)
 
 func load_gltf_segments(path: String, count: int):
 	var gltf_document = GLTFDocument.new()
 	var gltf_state = GLTFState.new()
 	
 	for i in range(1, count + 1):
-		var file_name = "fce41ff62f8a_" + str(i) + "-col.gltf"
+		var file_name = "e291f2784ca3_" + str(i) + ".gltf"
 		var full_path = path + file_name
 		print("loading " + file_name)
 		
